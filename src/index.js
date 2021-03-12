8c240165db2be53724fe0bf8a463f12d2fbae78f
NazarL <nazar.lebedenko@gmail.com>
2021-03-12 12:45:33 -0500

    import and html elements injection examples

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a test paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
